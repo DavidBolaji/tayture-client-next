476b9324ad21d7b391984cc280a2a55253a1b43e
Tunzymono <tife2020@gmail.com>
2024-04-14 16:52:04 +0100

    heading component style fix

--- pages/blog/components/HeadingDesc.tsx
+++ pages/blog/components/HeadingDesc.tsx
@@ -7,12 +7,12 @@ interface HeadingDescProps {
 
 const HeadingDesc = ({ heading, description }: HeadingDescProps) => {
   return (
-    <div className="relative flex flex-col sm:flex-row sm:items-end justify-between mb-10 md:mb-12 text-neutral-900 dark:text-neutral-50">
+    <div className="relative flex flex-col sm:flex-row sm:items-end justify-between mb-10 md:mb-12 text-neutral-900">
       <div className="max-w-2xl">
-        <h2 className="text-2xl md:text-3xl lg:text-4xl font-semibold">
+        <h2 className="text-2xl md:text-3xl lg:text-3xl font-[900]">
           {heading}
         </h2>
-        <span className="mt-2 md:mt-3 font-normal block text-base sm:text-xl text-neutral-500 dark:text-neutral-400">
+        <span className="mt-2 md:mt-3 font-normal block text-base sm:text-lg text-neutral-500">
           {/* the number of topics will be length of articles array + 1 */}
           {description}
         </span>
