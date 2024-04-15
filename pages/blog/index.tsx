82682a5eaf46e4d342d185cde26fb924aa06d1a1
Tunzymono <tife2020@gmail.com>
2024-04-15 14:19:26 +0100

    Section 5 popular post right cotent

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -16,6 +16,7 @@ import SmallArticleCard from './components/SmallArticleCard'
 import SmallArticleCardVertical from './components/SmallArticleCardVertical'
 import WidgetHeading from './components/WidgetHeading'
 import WidgetTags from './components/WidgetTags'
+import PopularPostCard from './components/PopularPostCard'
 
 const Blog = () => {
   return (
@@ -509,36 +510,66 @@ const Blog = () => {
                 {/* Right */}
                 <div className="right_cont w-full space-y-7 mt-24 lg:mt-0 lg:w-2/5 lg:pl-10 xl:pl-0 xl:w-1/3 ">
                   {/* Heading and Tags Container */}
-                  <div className="rounded-3xl overflow-hidden bg-neutral-100">
+                  <div className="rounded-3xl overflow-hidden bg-[rgb(242,242,242)]">
                     {/* heading */}
                     <WidgetHeading heading_text="Tags" view_all_link="#" />
 
                     {/* Tags Container */}
-                    <div className='flex flex-wrap p-4 xl:p-5'>
-                      <WidgetTags 
-                      tag_text='Admin'
-                      tag_link='#'
-                      tot_articles='20'
+                    <div className="flex flex-wrap p-4 xl:p-5">
+                      <WidgetTags
+                        tag_text="Admin"
+                        tag_link="#"
+                        tot_articles="20"
                       />
-                      <WidgetTags 
-                      tag_text='Educators'
-                      tag_link='#'
-                      tot_articles='10'
+                      <WidgetTags
+                        tag_text="Educators"
+                        tag_link="#"
+                        tot_articles="10"
                       />
-                      <WidgetTags 
-                      tag_text='Pupils'
-                      tag_link='#'
-                      tot_articles='15'
+                      <WidgetTags
+                        tag_text="Pupils"
+                        tag_link="#"
+                        tot_articles="15"
                       />
-                      <WidgetTags 
-                      tag_text='Parents'
-                      tag_link='#'
-                      tot_articles='25'
+                      <WidgetTags
+                        tag_text="Parents"
+                        tag_link="#"
+                        tot_articles="25"
                       />
-                      <WidgetTags 
-                      tag_text='Events'
-                      tag_link='#'
-                      tot_articles='20'
+                      <WidgetTags
+                        tag_text="Events"
+                        tag_link="#"
+                        tot_articles="20"
+                      />
+                    </div>
+                  </div>
+                  {/* Popular posts */}
+                  <div className="rounded-3xl overflow-hidden bg-[rgb(242,242,242)]">
+                    <WidgetHeading
+                      heading_text="Popular Posts"
+                      view_all_link="#"
+                    />
+                    {/* popular post posts cont */}
+                    <div className='flex flex-col divide-y divide-neutral-200'>
+                      <PopularPostCard
+                        popular_post_tag="#"
+                        popular_post_heading="Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nemo excepturi saepe."
+                        popular_post_image="https://img.freepik.com/free-photo/medium-shot-family-together-home_23-2149160316.jpg?t=st=1713113078~exp=1713116678~hmac=7e887d6b596123021b0dbf4862433f546d89337fba9852c23088f966df9853fb&w=360"
+                      />
+                      <PopularPostCard
+                        popular_post_tag="#"
+                        popular_post_heading="Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nemo excepturi saepe."
+                        popular_post_image="https://img.freepik.com/free-photo/medium-shot-family-together-home_23-2149160316.jpg?t=st=1713113078~exp=1713116678~hmac=7e887d6b596123021b0dbf4862433f546d89337fba9852c23088f966df9853fb&w=360"
+                      />
+                      <PopularPostCard
+                        popular_post_tag="#"
+                        popular_post_heading="Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nemo excepturi saepe."
+                        popular_post_image="https://img.freepik.com/free-photo/medium-shot-family-together-home_23-2149160316.jpg?t=st=1713113078~exp=1713116678~hmac=7e887d6b596123021b0dbf4862433f546d89337fba9852c23088f966df9853fb&w=360"
+                      />
+                      <PopularPostCard
+                        popular_post_tag="#"
+                        popular_post_heading="Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nemo excepturi saepe."
+                        popular_post_image="https://img.freepik.com/free-photo/medium-shot-family-together-home_23-2149160316.jpg?t=st=1713113078~exp=1713116678~hmac=7e887d6b596123021b0dbf4862433f546d89337fba9852c23088f966df9853fb&w=360"
                       />
                     </div>
                   </div>
