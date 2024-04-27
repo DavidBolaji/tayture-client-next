610c5208cbd57989dadc524643fa13e0e4e862fc
David <odavidbolaji14@gmail.com>
2024-04-27 08:36:14 +0100

    changed nodemailer settings

--- mail/transporter.ts
+++ mail/transporter.ts
@@ -5,11 +5,15 @@ const transporter = nodemailer.createTransport({
   service: "zoho",
   host: 'smtpro.zoho.in',
   port: 465,
-  secure: true,
+  secure: false,
   auth: {
     user: 'hello@tayture.com',
     pass: 'E5&4koG#+r[}' 
     // 
+  },
+  tls: {
+    // do not fail on invalid certs
+    rejectUnauthorized: false,
   },
 })
 
@@ -17,11 +21,15 @@ export const transporter2 = nodemailer.createTransport({
   service: "zoho",
   host: 'smtpro.zoho.in',
   port: 465,
-  secure: true,
+  secure: false,
   auth: {
     user: 'hello@tayture.com',
     pass: 'E5&4koG#+r[}',
     // E5&4koG#+r[}
+  },
+  tls: {
+    // do not fail on invalid certs
+    rejectUnauthorized: false,
   },
 })
 
