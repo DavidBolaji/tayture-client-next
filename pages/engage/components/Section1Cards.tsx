a126f4912e8ea9c08a6774f285c13da1f1f2976a
Tunzymono <tife2020@gmail.com>
2024-05-02 12:13:05 +0100

    Fixed Responsiveness

--- pages/engage/components/Section1Cards.tsx
+++ pages/engage/components/Section1Cards.tsx
@@ -9,7 +9,7 @@ function Section1Cards() {
       heading: 'Watch SchoolRoom episodes',
       icon_link_text: 'See episodes',
       href: '#',
-      is_play_icon : true,
+      is_play_icon: true,
     },
     {
       image_src:
@@ -17,7 +17,7 @@ function Section1Cards() {
       heading: 'Join the top educators in Nigeria(TEN) Group',
       icon_link_text: 'Join',
       href: '#',
-      is_play_icon : false,
+      is_play_icon: false,
     },
     {
       image_src:
@@ -25,7 +25,7 @@ function Section1Cards() {
       heading: 'Get educator scholarship for teachers',
       icon_link_text: 'Apply',
       href: '#',
-      is_play_icon : false,
+      is_play_icon: false,
     },
     {
       image_src:
@@ -33,7 +33,7 @@ function Section1Cards() {
       heading: 'Recommend an Educator for recognition',
       icon_link_text: 'Yes',
       href: '#',
-      is_play_icon : false,
+      is_play_icon: false,
     },
     {
       image_src:
@@ -41,7 +41,7 @@ function Section1Cards() {
       heading: 'Partner with us to honour top Educators',
       icon_link_text: 'Yes',
       href: '#',
-      is_play_icon : false,
+      is_play_icon: false,
     },
     {
       image_src:
@@ -49,7 +49,7 @@ function Section1Cards() {
       heading: 'Watch Educator recognition video',
       icon_link_text: 'See episodes',
       href: '#',
-      is_play_icon : true,
+      is_play_icon: true,
     },
     {
       image_src:
@@ -57,7 +57,7 @@ function Section1Cards() {
       heading: 'Get a Job in a school',
       icon_link_text: 'See jobs',
       href: '#',
-      is_play_icon : false,
+      is_play_icon: false,
     },
     {
       image_src:
@@ -65,24 +65,25 @@ function Section1Cards() {
       heading: 'Hire staff for your school',
       icon_link_text: 'Post a job',
       href: '#',
-      is_play_icon : false,
+      is_play_icon: false,
     },
   ]
   return (
-    <div className="z-0 bg-neutral-100 rounded-[40px] relative p-10 md:p-12 mt-10">
-      {/* grid cont */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
-        {cardDetails.map((card, index) => (
-          <CardLandscape
-            key={index}
-            icon_link_text={card.icon_link_text}
-            heading={card.heading}
-            image_src= {card.image_src}
-            href={card.href}
-            is_play_icon = {card.is_play_icon}
-          />
-        ))}
-      </div>
+    <div className="relative ">
+
+        {/* grid cont */}
+        <div className="  grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8 mt-8">
+          {cardDetails.map((card, index) => (
+            <CardLandscape
+              key={index}
+              icon_link_text={card.icon_link_text}
+              heading={card.heading}
+              image_src={card.image_src}
+              href={card.href}
+              is_play_icon={card.is_play_icon}
+            />
+          ))}
+        </div>
     </div>
   )
 }
--- pages/engage/index.tsx
+++ pages/engage/index.tsx
@@ -8,12 +8,13 @@ function Engage() {
   return (
     <>
       <div className="h-[100vh] overflow-auto">
-        <div>
-          <Wrapper>
+        <Wrapper>
+
+        <div className='relative py-16 '>
+            <div className='absolute inset-y-0 w-screen xl:max-w-[1340px] 2xl:max-w-screen-2xl left-1/2 transform -translate-x-1/2 xl:rounded-[40px] z-0 bg-neutral-100'></div>
             <Section1Cards/>
-          </Wrapper>
         </div>
-        <div className="h-[100vh]"></div>
+        </Wrapper>
         <Footer />
       </div>
     </>
