28ff4ac295f68a71318101b14b80a3beb01017e8
davidbolaji <odavidbolaji14@gmail.com>
2024-07-25 23:41:55 +0100

    completed csv download for applied users

---
+++ pages/dashboard/admin/hooks/useDownloadCSV.tsx
@@ -0,0 +1,33 @@
+import { useMutation } from '@tanstack/react-query'
+import { Axios } from '@/request/request'
+import { saveAs } from 'file-saver'
+import { FormikHelpers } from 'formik';
+
+interface ICSV {
+    start: string;
+    end: string
+}
+
+export const useDownloadCSV = () => {
+  const { isPending, mutate } = useMutation({
+    mutationKey: ['applied csv'],
+    mutationFn: async (arg:ICSV ) => {
+      const response = await Axios.post(
+        '/apply/csv',
+        { ...arg },
+        { responseType: 'blob' },
+      )
+      return response.data
+    },
+    onSuccess: (data) => {
+      const blob = new Blob([data], { type: 'text/csv;charset=utf-8;' })
+      saveAs(blob, 'applied.csv')
+    },
+  })
+
+  const handleSubmit = (values:ICSV, { resetForm }: FormikHelpers<ICSV>) => {
+    mutate(values)
+  }
+
+  return { isPending, handleSubmit }
+}
